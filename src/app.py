f0fdfc556e75e4adb4df24300e552248dbbf085c
Admin <schule@pop-os.localdomain>
2021-10-05 17:46:08 +0200

    added app.py

--- src/app.py
+++ src/app.py
@@ -8,5 +8,15 @@ class PersonDataClass:
 
 p = PersonDataClass("Egon", "Koch", "06.09.2004")
 n = PersonDataClass("Jamie", "Holzer", "27.06.2005")
-print(p,n)
+
+persons = [p, n]
+for personen in persons:
+    print(personen)
+
+def printPersonen(persons):
+    persons = [p, n]
+    for personen in persons:
+        print(personen)
+    if name == main:
+        printPersons(persons)
 
